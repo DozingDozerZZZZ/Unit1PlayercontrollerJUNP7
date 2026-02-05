7908c898b01506109f015211f07429218631ba6a
DozingDozerZZZZ <763664@student.dusd.net>
2026-02-05 15:28:33 -0800

    Completed player controller car on to challenge 1 now
    rough draft finished

--- JunUnit1PlayerControllerP7/Assets/Scripts/PlayerController.cs
+++ JunUnit1PlayerControllerP7/Assets/Scripts/PlayerController.cs
@@ -3,6 +3,9 @@ using UnityEngine;
 public class PlayerController : MonoBehaviour
 {
     public float speed = 5.0f;
+    public float turnSpeed = 2;
+    public float horizontalInput = 2;
+    public float forwardInput;
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
@@ -12,7 +15,13 @@ public class PlayerController : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        //Move the car forward
-        transform.Translate(Vector3.forward * Time.deltaTime * speed);
+        horizontalInput = Input.GetAxis("Horizontal");
+       
+        forwardInput = Input.GetAxis("Vertical");
+
+        //Can move the car forward or backward
+        transform.Translate(Vector3.forward * Time.deltaTime * speed * forwardInput);
+        //Rotates car based on input
+        transform.Rotate(Vector3.up ,turnSpeed * horizontalInput *Time.deltaTime);
     }
 }
