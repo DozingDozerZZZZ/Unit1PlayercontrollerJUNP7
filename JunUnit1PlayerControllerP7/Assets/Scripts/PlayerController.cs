c16056bc5d380c9920a3af859b231a4accbe5a0a
DozingDozerZZZZ <763664@student.dusd.net>
2026-02-11 15:22:53 -0800

    Trying to get a the camera switch to work

--- JunUnit1PlayerControllerP7/Assets/Scripts/PlayerController.cs
+++ JunUnit1PlayerControllerP7/Assets/Scripts/PlayerController.cs
@@ -2,10 +2,18 @@ using UnityEngine;
 
 public class PlayerController : MonoBehaviour
 {
+    //Cameras
+    public Camera mainCamera;
+    public Camera hoodCamera;
+    public KeyCode switchKey;
+
     public float speed = 5.0f;
     public float turnSpeed = 2;
     public float horizontalInput = 2;
     public float forwardInput;
+
+    
+
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
@@ -23,5 +31,11 @@ public class PlayerController : MonoBehaviour
         transform.Translate(Vector3.forward * Time.deltaTime * speed * forwardInput);
         //Rotates car based on input
         transform.Rotate(Vector3.up ,turnSpeed * horizontalInput *Time.deltaTime);
+
+        if(forwardInput.GetKeyDown(switchKey))
+        {
+            mainCamera.enabled = !mainCamera.enabled;
+            hoodCamera.enabled = !hoodCamera.enabled;
+        }
     }
 }
